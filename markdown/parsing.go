4f10f8cff0385726e6d2d9cb75f81db16eb9c3a5
Frederic Vogels <frederic.vogels@gmail.com>
2025-11-29 11:55:03 +0100

    markdown parsing

--- markdown/parsing.go
+++ markdown/parsing.go
@@ -1,12 +1,20 @@
 package markdown
 
 import (
+	"os"
+
 	"github.com/yuin/goldmark"
 	meta "github.com/yuin/goldmark-meta"
 	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/text"
 )
+
+type MarkdownFile struct {
+	Path     string
+	AST      ast.Node
+	Metadata map[string]any
+}
 
 func Parse(source []byte) (ast.Node, map[string]any) {
 	markdown := goldmark.New(
@@ -17,8 +25,24 @@ func Parse(source []byte) (ast.Node, map[string]any) {
 	context := parser.NewContext()
 
 	reader := text.NewReader(source)
-	document := markdown.Parser().Parse(reader, parser.WithContext(context))
+	ast := markdown.Parser().Parse(reader, parser.WithContext(context))
 	metadata := meta.Get(context)
 
-	return document, metadata
+	return ast, metadata
+}
+
+func ParseFile(path string) (*MarkdownFile, error) {
+	source, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	ast, metadata := Parse(source)
+	markdownFile := MarkdownFile{
+		Path:     path,
+		AST:      ast,
+		Metadata: metadata,
+	}
+
+	return &markdownFile, nil
 }
