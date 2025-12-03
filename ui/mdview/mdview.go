7f88f94770c8a5de3ced81f3b29f25cc959a6289
Frederic Vogels <frederic.vogels@gmail.com>
2025-12-03 11:23:51 +0100

    refactoring

--- ui/mdview/mdview.go
+++ ui/mdview/mdview.go
@@ -32,33 +32,10 @@ func (model Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch message := message.(type) {
 	case tea.WindowSizeMsg:
-		model.size = util.Size{
-			Width:  message.Width,
-			Height: message.Height,
-		}
-		return model, nil
+		return model.onResize(message)
 
 	case MsgSetSource:
-		width := model.size.Width
-
-		command := func() tea.Msg {
-			renderer, err := glamour.NewTermRenderer(
-				glamour.WithAutoStyle(),
-				glamour.WithWordWrap(width-2),
-			)
-			if err != nil {
-				panic("failed to create markdown renderer")
-			}
-			renderedMarkdown, err := renderer.Render(message.Source)
-			if err != nil {
-				panic("failed to render markdown file")
-			}
-			return msgRenderingDone{
-				renderedMarkdown: renderedMarkdown,
-			}
-		}
-
-		return model, command
+		return model.onSetSource(message)
 
 	case msgRenderingDone:
 		model.renderedMarkdown = message.renderedMarkdown
@@ -72,4 +49,35 @@ func (model Model) View() string {
 	borderStyle := lipgloss.NewStyle().Width(model.size.Width - 2).Height(model.size.Height - 2).Border(lipgloss.DoubleBorder())
 	innerStyle := lipgloss.NewStyle().MaxWidth(model.size.Width - 2).MaxHeight(model.size.Height - 2)
 	return borderStyle.Render(innerStyle.Render(model.renderedMarkdown))
+}
+
+func (model Model) onSetSource(message MsgSetSource) (tea.Model, tea.Cmd) {
+	width := model.size.Width
+
+	command := func() tea.Msg {
+		renderer, err := glamour.NewTermRenderer(
+			glamour.WithAutoStyle(),
+			glamour.WithWordWrap(width-2),
+		)
+		if err != nil {
+			panic("failed to create markdown renderer")
+		}
+		renderedMarkdown, err := renderer.Render(message.Source)
+		if err != nil {
+			panic("failed to render markdown file")
+		}
+		return msgRenderingDone{
+			renderedMarkdown: renderedMarkdown,
+		}
+	}
+
+	return model, command
+}
+
+func (model Model) onResize(message tea.WindowSizeMsg) (tea.Model, tea.Cmd) {
+	model.size = util.Size{
+		Width:  message.Width,
+		Height: message.Height,
+	}
+	return model, nil
 }
